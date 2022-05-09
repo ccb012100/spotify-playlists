ea6b0a48f650c6b407535fbc329ff5700a87054f
Christopher Bocardo <ccb012100@users.noreply.github.com>
2022-05-09 13:48:10 -0500

    use full path to starredmusic.tsv

--- search.sh
+++ search.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 function sm() {
-    rg -i "$1" starredmusic.tsv | \
+    rg -i "$1" ~/ccb012100/starred_music/starredmusic.tsv | \
     awk -F '\t' '{
         printf "%s\t%s\t%3d\t%s\t%s\n",
         $1, $2, $3, substr($4,1,4), substr($5,1,10) }' | \
