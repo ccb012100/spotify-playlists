649f24b132171d8944d4f9947f4fe3a0bd87d619
Christopher Bocardo <ccb012100@users.noreply.github.com>
2022-05-14 16:24:37 -0500

    change args param reference to make searching easier

--- search.sh
+++ search.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 function sm() {
-    rg -i "$1" ~/ccb012100/starred_music/starredmusic.tsv | \
+    # "${*}" will group all the args into a single quoted string, so we don't
+    # have to wrap the search in quotes on the command line, e.g. we can enter
+    # <sm Allman Brothers> instead of <sm "Allman Brothers">
+    rg -i "${*}" ~/ccb012100/starred_music/starredmusic.tsv | \
     awk -F '\t' '{
         printf "%s\t%s\t%3d\t%s\t%s\n",
         $1, $2, $3, substr($4,1,4), substr($5,1,10) }
