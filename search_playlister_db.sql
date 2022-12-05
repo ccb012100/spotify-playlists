544f3db5918cdb56111580e837c6d7c047aa8c6d
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2022-12-05 16:50:23 -0600

    change output mode and truncate added_at

--- search_playlister_db.sql
+++ search_playlister_db.sql
@@ -1,9 +1,9 @@
 -- search database by artist+album names
 -- sqlite3 --readonly "$HOME/playlister.db" ".param set :term $term" ".read search_playlister_db.sql"
 
-.mode box
+.mode column
 
-select art.name as artists, a.name as album, a.total_tracks as tracks, substr(a.release_date, 1, 4) as release_date, p.name, pt.added_at from Album a
+select art.name as artists, a.name as album, a.total_tracks as tracks, substr(a.release_date, 1, 4) as release_date, p.name, substr(pt.added_at, 1, 10) as added_at from Album a
 join albumartist aa on aa.album_id = a.id
 join artist art on art.id = aa.artist_id
 join track t on t.album_id = a.id
