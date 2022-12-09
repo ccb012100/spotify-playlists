eb8ee58889ce0b27be3afc5ac89996769b6801fb
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2022-12-09 17:48:00 -0600

    add 'sync' command and improve messages

--- search.sh
+++ search.sh
@@ -6,26 +6,44 @@ REPO=$(dirname -- "$(readlink -f -- "$0")")
 
 function __sm_validate_search_term() {
     if [ ${#1} -eq 0 ]; then
-        echo "Error: must provide search term"
+        echo -e "Error: must provide search term\n"
+        __sm_print_usage
     elif [ ${#1} -lt 4 ]; then
         echo "Error: search term must be at least 4 chars"
     fi
 }
 
 function __sm_print_usage() {
-    echo -e 'USAGE:\n\tsm last NUMBER\n\tsm db SEARCH_TERM\n\tsm SEARCH_TERM'
+    echo -e 'USAGE:\n\tsm SEARCH_TERM\n\tsm db SEARCH_TERM\n\tsm last NUMBER\n\tsm sync [db | tsv]'
 }
 
 function sm() {
-
     case $1 in
-    -h)
+    # print usage info
+    help | -h)
         __sm_print_usage
         ;;
+    # sync sqlite db and tsv file
+    sync | update)
+        case $2 in
+        db)
+            dotnet run --project "$HOME/ccb012100/playlister/Playlister.csproj"
+            ;;
+        tsv)
+            "$REPO/update_spreadsheet.py"
+            ;;
+        *)
+            echo "Error: you must use '$1 db' or '$1 tsv'"
+            return 1
+            ;;
+        esac
+        ;;
+    # update tsv file manually
     rs)
         shift
         starred_music_rs # binary located in ~/bin/
         ;;
+    # get last N albums added to starred Spotify playlists
     last)
         shift
         limit=10 # default to 10
@@ -34,6 +52,7 @@ function sm() {
         fi
         sqlite3 --readonly "$HOME/playlister.db" ".param init" ".param set :limit $limit" ".read get_last_x_additions.sql"
         ;;
+    # search sqlite DB
     db)
         shift
         ERROR="$(__sm_validate_search_term "${*}")"
@@ -46,6 +65,7 @@ function sm() {
             sqlite3 --readonly "$HOME/playlister.db" ".param init" ".param set :term '${*}'" ".read $REPO/search_playlister_db.sql"
         fi
         ;;
+    # search tsv file
     *)
         # "${*}" will group all the args into a single quoted string, so we
         # don't have to wrap the search in quotes on the command line, e.g. we
@@ -55,6 +75,7 @@ function sm() {
             echo >&2 "$ERROR"
             return 1
         fi
+        echo "search for '$1':"
         rg -i "${*}" "$REPO/starredmusic.tsv" |
             awk -F '\t' '{ printf "%s\t%s\t%3d\t%s\t%s\n", $1, $2, $3, substr($4,1,4), substr($5,1,10) } END{print "----\n" NR " matches"}' | sort | column -t -s $'\t'
         ;;
