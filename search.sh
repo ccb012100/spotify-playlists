62d36fc0fad2aac62028861140ff208330d27c5f
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2022-12-14 17:49:40 -0600

    add sorting options; refactor whole script

--- search.sh
+++ search.sh
@@ -2,7 +2,7 @@
 
 # location of the repository this script is hosted in
 # source: https://stackoverflow.com/a/1482133
-REPO=$(dirname -- "$(readlink -f -- "$0")")
+SM_REPO=$(dirname -- "$(readlink -f -- "$0")")
 
 function __sm_validate_search_term() {
     if [ ${#1} -eq 0 ]; then
@@ -12,45 +12,65 @@ function __sm_validate_search_term() {
         echo "Error: search term must be at least 4 chars"
     fi
 }
-
 function __sm_print_usage() {
-    echo -e 'USAGE:\n\tsm SEARCH_TERM\n\tsm db SEARCH_TERM\n\tsm last NUMBER\n\tsm sync [db | tsv]'
+    echo -e 'USAGE:\n\tsm SEARCH_TERM'
+    echo -e '\tsm sort [date | artist] SEARCH_TERM'
+    echo -e '\tsm SEARCH_TERM'
+    echo -e '\tsm db SEARCH_TERM'
+    echo -e '\tsm last NUMBER'
+    echo -e '\tsm sync [db | tsv] SEARCH_TERM'
+}
+function __sm_format_matches() {
+    if [ -p /dev/stdin ]; then
+        awk -F '\t' '{ printf "%s\t%s\t%3d\t%s\t%s\n", $1, $2, $3, substr($4,1,4), substr($5,1,10) }' |
+            column --table --separator $'\t' --output-separator $'\t'
+    else
+        echo "Error: no input was found on stdin"
+    fi
+}
+function __sm_sort() {
+    if [ -p /dev/stdin ]; then
+        sort -t $'\t' -k "$1","$1" </dev/stdin |
+            awk '{ print } END { print "\n\t" NR " matches" }'
+    else
+        echo "Error: no input was found on stdin"
+    fi
+}
+function __sm_sort_by_release() {
+    __sm_sort 4
+}
+function __sm_sort_by_artist() {
+    __sm_sort 1
+}
+function __sm_sort_by_album() {
+    __sm_sort 2
+}
+function __sm_search() {
+    # "${*}" will group all the args into a single quoted string, so we
+    # don't have to wrap the search in quotes on the command line, e.g. we
+    # can enter `sm Allman Brothers`` instead of `sm "Allman Brothers"`
+    ERROR="$(__sm_validate_search_term "${*}")"
+    if [[ -n "$ERROR" ]]; then
+        echo >&2 "$ERROR"
+        return 1
+    fi
+    rg -i "${*}" "$SM_REPO/starredmusic.tsv"
+}
+function __sm_default_search() {
+    # sort by album title
+    __sm_search "${*}" | __sm_format_matches | __sm_sort_by_album
 }
 
 function sm() {
     case $1 in
     # print usage info
-    help | -h)
+    -h | --help | help)
         __sm_print_usage
         ;;
-    # sync sqlite db and tsv file
-    sync | update)
-        case $2 in
-        db)
-            dotnet run --project "$HOME/ccb012100/playlister/Playlister.csproj"
-            ;;
-        tsv)
-            "$REPO/update_spreadsheet.py"
-            ;;
-        *)
-            echo "Error: you must use '$1 db' or '$1 tsv'"
-            return 1
-            ;;
-        esac
-        ;;
-    # update tsv file manually
-    rs)
+    # pass through all args to default search
+    verbatim)
         shift
-        starred_music_rs # binary located in ~/bin/
-        ;;
-    # get last N albums added to starred Spotify playlists
-    last)
-        shift
-        limit=10 # default to 10
-        if [[ -n "$1" ]] && [[ "$1" -gt 0 ]]; then
-            limit=$1
-        fi
-        sqlite3 --readonly "$HOME/playlister.db" ".param init" ".param set :limit $limit" ".read get_last_x_additions.sql"
+        __sm_default_search "${*}"
         ;;
     # search sqlite DB
     db)
@@ -62,31 +82,65 @@ function sm() {
         else
             [[ -n "${*}" ]]
             echo "Matches for '${*}':"
-            sqlite3 --readonly "$HOME/playlister.db" ".param init" ".param set :term '${*}'" ".read $REPO/search_playlister_db.sql"
+            sqlite3 --readonly "$HOME/playlister.db" ".param init" ".param set :term '${*}'" ".read $SM_REPO/search_playlister_db.sql"
         fi
         ;;
-    # search tsv file
-    *)
-        # "${*}" will group all the args into a single quoted string, so we
-        # don't have to wrap the search in quotes on the command line, e.g. we
-        # can enter `sm Allman Brothers`` instead of `sm "Allman Brothers"`
-        ERROR="$(__sm_validate_search_term "${*}")"
-        if [[ -n "$ERROR" ]]; then
-            echo >&2 "$ERROR"
-            return 1
+        # search tsv file and sort by release date
+    # get last N albums added to starred Spotify playlists
+    last)
+        limit=10 # default to 10
+        if [[ -n "$2" ]] && [[ "$2" -gt 0 ]]; then
+            limit=$2
         fi
-        echo "search for '$*':"
-        rg -i "${*}" "$REPO/starredmusic.tsv" |
-            awk -F '\t' '{ printf "%s\t%s\t%3d\t%s\t%s\n", $1, $2, $3, substr($4,1,4), substr($5,1,10) } END{print "----\n" NR " matches"}' | sort | column --table --separator $'\t' --output-separator $'\t'
+        sqlite3 --readonly "$HOME/playlister.db" ".param init" ".param set :limit $limit" ".read get_last_x_additions.sql"
+        ;;
+        # update tsv file manually
+    rs)
+        starred_music_rs # binary located in ~/bin/
+        ;;
+    sort)
+        shift
+        echo -e "\tSearch for '${*}':\n"
+        case $1 in
+        date)
+            shift
+            __sm_search "${*}" | __sm_format_matches | __sm_sort_by_release
+            ;;
+            # search tsv file and sort by artist
+        artist)
+            shift
+            __sm_search "${*}" | __sm_format_matches | __sm_sort_by_release
+            ;;
+        album)
+            shift
+            __sm_search "${*}" | __sm_format_matches | __sm_sort_by_album
+            ;;
+        *)
+            echo -e "'sort' must be followed by [date | artist]\n"
+            __sm_print_usage
+            ;;
+        esac
+        ;;
+    sync)
+        case $2 in
+        # sync sqlite db
+        db)
+            dotnet run --project "$HOME/ccb012100/playlister/Playlister.csproj"
+            ;;
+            # sync tsv file
+        tsv)
+            "$SM_REPO/update_spreadsheet.py"
+            ;;
+        *)
+            echo "Error: you must use 'sync db' or 'sync tsv'"
+            return 1
+            ;;
+        esac
+        ;;
+    # search tsv file with default search
+    *)
+        echo -e "\tSearch for '${*}':\n"
+        __sm_default_search "${*}"
         ;;
     esac
-}
-
-function sm_sort_by_release() {
-    if [ -p /dev/stdin ]; then
-        sort -t $'\t' -k 4,4 -n </dev/stdin
-
-    else
-        echo "Error: no input was found on stdin"
-    fi
 }
