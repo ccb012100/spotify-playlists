0f25e43ac33670deeaffe4636386e5ad25a56d31
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2022-12-16 22:16:41 -0600

    format

--- sql/get_last_x_additions.sql
+++ sql/get_last_x_additions.sql
@@ -1,19 +1,27 @@
 -- view last $LIMIT tracks, in order of being added to the db
 -- call in CLI: `sqlite3 --readonly "$HOME/playlister.db" ".param set :limit $LIMIT" ".read get_last_x_additions.sql"`
-
 .mode tabs
 
-select * from
-(select art.name, a.name, a.total_tracks, substr(a.release_date, 1, 4), pt.added_at, p.name from Album a
-join albumartist aa on aa.album_id = a.id
-join artist art on art.id = aa.artist_id
-join track t on t.album_id = a.id
-join playlisttrack pt on pt.track_id = t.id
-join playlist p on p.id = pt.playlist_id
-where p.name like 'starred%'
-group by pt.added_at, art.id
-order by pt.added_at DESC
-limit CAST(:limit as NUMBER))
+select *
+from (
+        select art.name,
+            a.name,
+            a.total_tracks,
+            substr(a.release_date, 1, 4),
+            pt.added_at,
+            p.name
+        from Album a
+            join albumartist aa on aa.album_id = a.id
+            join artist art on art.id = aa.artist_id
+            join track t on t.album_id = a.id
+            join playlisttrack pt on pt.track_id = t.id
+            join playlist p on p.id = pt.playlist_id
+        where p.name like 'starred%'
+        group by pt.added_at,
+            art.id
+        order by pt.added_at DESC
+        limit CAST(:limit as NUMBER)
+    )
 order by added_at;
 
 .exit
--- sql/search_playlister_db.sql
+++ sql/search_playlister_db.sql
@@ -1,16 +1,24 @@
 -- search database by artist+album names
 -- sqlite3 --readonly "$HOME/playlister.db" ".param set :term $term" ".read search_playlister_db.sql"
-
 .mode column
 
-select art.name as artists, a.name as album, a.total_tracks as tracks, substr(a.release_date, 1, 4) as release_date, p.name, substr(pt.added_at, 1, 10) as added_at from Album a
-join albumartist aa on aa.album_id = a.id
-join artist art on art.id = aa.artist_id
-join track t on t.album_id = a.id
-join playlisttrack pt on pt.track_id = t.id
-join playlist p on p.id = pt.playlist_id
-where art.name like format('%%%s%%', :term) or a.name like format('%%%s%%', :term)
-group by p.id, a.id
-order by art.name COLLATE NOCASE ASC, a.name COLLATE NOCASE ASC;
+select art.name as artists,
+    a.name as album,
+    a.total_tracks as tracks,
+    substr(a.release_date, 1, 4) as release_date,
+    p.name,
+    substr(pt.added_at, 1, 10) as added_at
+from Album a
+    join albumartist aa on aa.album_id = a.id
+    join artist art on art.id = aa.artist_id
+    join track t on t.album_id = a.id
+    join playlisttrack pt on pt.track_id = t.id
+    join playlist p on p.id = pt.playlist_id
+where art.name like format('%%%s%%', :term)
+    or a.name like format('%%%s%%', :term)
+group by p.id,
+    a.id
+order by art.name COLLATE NOCASE ASC,
+    a.name COLLATE NOCASE ASC;
 
 .headers off
