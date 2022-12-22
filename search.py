5d52b13c3240328af58bdcc039f354b1ab82e972
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2022-12-22 08:35:25 -0600

    update sql, use 'Rich' display library
    This modifies the SQL to use a single query to get both AlbumArtists and
    TrackArtists for a given search. They come back as 1 entry per
    (album, artist, track artist, playlist) pairing.
    
    Also switched to the Rich python package for displaying output. Main
    reason for switching was that tables will automatically adjust to the
    terminal width.

--- search.py
+++ search.py
@@ -6,30 +6,26 @@
 
     USAGE: ./search.py SEARCH_TERM
 '''
-from copy import deepcopy
-from colorama import Fore, Style
 from enum import Enum
 from pathlib import Path
-from tabulate import tabulate
+from rich.table import Table, Column
+from rich import print
 from urllib.request import pathname2url
-import os
 import sqlite3
 import sys
 
 sql_db = str(Path.home() / 'playlister.db')
 
-
-class Columns:
-    added = 'added'
-    album = 'album'
-    album_id = 'album_id'
-    artists = 'artists'
-    playlist = 'playlist'
-    playlist_id = 'playlist_id'
-    released = 'released'
-    single_artist = 'artist'
-    track_artists = 'track_artists'
-    tracks = 'tracks'
+ADDED = 'added'
+ALBUM = 'album'
+ALBUM_ID = 'album_id'
+ARTIST = 'artist'
+PLAYLIST = 'playlist'
+PLAYLIST_ID = 'playlist_id'
+RELEASED = 'released'
+SINGLE_ARTIST = 'artist'
+TRACK_ARTIST = 'track_artist'
+TRACKS = 'tracks'
 
 
 def get_sqlite_cursor(db_file: str) -> sqlite3.Cursor:
@@ -48,154 +44,164 @@ def get_sqlite_cursor(db_file: str) -> sqlite3.Cursor:
 Message_Level = Enum('Message_Level', ['ERROR', 'SUCCESS', 'WARNING', 'INFO'])
 
 
-def print_error(message: str):
+def print_error(message: str) -> None:
     print_message(Message_Level.ERROR, f'ERROR: {message}')
 
 
-def print_success(message: str):
+def print_success(message: str) -> None:
     print_message(Message_Level.SUCCESS, message)
 
 
-def print_warning(message: str):
+def print_warning(message: str) -> None:
     print_message(Message_Level.WARNING, message)
 
 
-def print_info(message: str):
+def print_info(message: str) -> None:
     print_message(Message_Level.INFO, message)
 
 
-def print_message(message_level, message: str):
-    message_level_colors = {
-        Message_Level.ERROR: Fore.RED + Style.BRIGHT,
-        Message_Level.SUCCESS: Fore.GREEN + Style.BRIGHT,
-        Message_Level.WARNING: Fore.YELLOW,
-        Message_Level.INFO: Fore.BLUE,
-    }
-
-    print(f'{message_level_colors.get(message_level, "")}{message}{Style.RESET_ALL}')
+def print_message(message_level, message: str) -> None:
+    print(message)
 
 
-def create_album_dict(db_rows: list) -> dict:
-    ''' Create dict in the form { album_id: { album } } '''
+def create_album_dict(db_rows: list) -> dict[str, dict[str, object]]:
+    ''' Create dict in the form
+        ```
+        {
+            [album_id: str] : {
+                album_id: str,
+                artist: set(str),
+                track_artist: set(str),
+                album: str,
+                tracks: number,
+                released: 'YYYY',
+                added: 'YYYY-MM-DD',
+                playlist: str,
+            }
+        }
+        ```
+    '''
     albums = {}
 
+    alb_artists = {}
+
     for row in db_rows:
-        albums[row[Columns.album_id]] = {k: row[k]
-                                         for k in row.keys() if k not in [Columns.album_id, Columns.playlist_id]}
+        alb = {k: row[k] for k in row.keys()
+               if k not in [ALBUM_ID, PLAYLIST_ID]}
+
+        id = row[ALBUM_ID]
+
+        artist = alb[ARTIST].strip()
+        track_artist = alb[TRACK_ARTIST].strip()
+
+        if id not in albums:
+            albums[id] = alb
+            alb_artists[id] = {}
+            alb_artists[id][ARTIST] = []
+            alb_artists[id][TRACK_ARTIST] = []
+
+        alb_artists[id][ARTIST].append(artist)
+
+        if track_artist != artist:
+            alb_artists[id][TRACK_ARTIST].append(track_artist)
+
+    for id in albums:
+        albums[id][ARTIST] = set(alb_artists[id][ARTIST])
+        albums[id][TRACK_ARTIST] = set(alb_artists[id][TRACK_ARTIST])
 
     return albums
 
 
-def search_by_track_artist(cursor: sqlite3.Cursor, search_term: str) -> dict:
-    cursor.execute(track_artist_query, [search_term])
+def search_by_artist(cursor: sqlite3.Cursor, search_term: str) -> dict:
+    cursor.execute(build_query("WHERE x.artist LIKE FORMAT('%%%s%%', ?) OR y.track_artist LIKE FORMAT('%%%s%%', ?)"),
+                   [search_term, search_term])
 
-    return create_album_dict(cursor.fetchall())
+    rows = cursor.fetchall()
 
-
-def search_by_album_artist(cursor: sqlite3.Cursor, search_term: str, trackartist_albums: dict) -> dict:
-    query = build_album_artist_query(trackartist_albums)
-
-    params = [search_term]
-    params.extend(t for t in trackartist_albums)
-
-    cursor.execute(query, params)
-
-    return create_album_dict(cursor.fetchall())
+    return create_album_dict(rows)
 
 
-def merge_album_data(track_matches: dict, albums: dict) -> list:
-    ''' Add track artist data from `trackartists_albums` to `albumartist_albums` '''
-    merged_albums = []
+def format_albums_for_table(albums: dict[str, dict]) -> list[dict[str, object]]:
+    ''' Format dict to list in format:
+        ```
+        [
+            {
+                artist: list[str],
+                track_artist: list[str],
+                album: str,
+                tracks: number,
+                released: 'YYYY',
+                added: 'YYYY-MM-DD',
+                playlist: str
+            }
+        ]
+        ```
+    '''
+    merged = []
 
-    for album_id in albums:
-        album = deepcopy(albums[album_id])
-        artists = [a.strip() for a in album[Columns.artists].split(';')]
+    for id in albums:
+        album = albums[id]
+        formatted = {k: album[k] for k in album.keys() if k != ALBUM_ID}
+        formatted[ARTIST] = '; '.join(album[ARTIST])
+        formatted[TRACK_ARTIST] = '; '.join(album[TRACK_ARTIST])
+        merged.append(formatted)
 
-        if album_id not in track_matches:
-            continue
-
-        t_album = track_matches[album_id]
-
-        track_artists = [t.strip()
-                         for t in t_album[Columns.track_artists].split(';')]
-
-        album[Columns.track_artists] = '; '.join(
-            [t for t in track_artists if t not in artists])
-
-        merged_albums.append(album)
-
-    return merged_albums
+    return merged
 
 
-def print_results(albums: list) -> None:
+def print_results(albums: list, search_term: str) -> None:
     if not albums:
         print_warning("\n\tThere were 0 matches")
         return
 
-    headers = [
-        Columns.artists,
-        Columns.track_artists,
-        Columns.album,
-        Columns.tracks,
-        Columns.released,
-        Columns.playlist,
-        Columns.added
-    ]
+    table = Table(Column(header='Artists', justify='left', style='blue'),
+                  Column(header='Track Artists', justify='left', style='red'),
+                  Column(header='Album', justify='left', style='green'),
+                  Column(header='# Tracks', justify='right', style='yellow'),
+                  Column(header='Released', justify='center', style='white'),
+                  Column(header='Playlist', justify='left', style='blue'),
+                  Column(header='Added On', justify='center', style='red'),
+                  title=f'Matches for "{search_term}"', show_lines=True)
 
-    print()
-    # reorder columns
-    # see: https://github.com/astanin/python-tabulate/issues/40#issuecomment-595225936
-    print(tabulate([[a.get(k, 0) for k in headers] for a in albums], headers=headers,
-          maxcolwidths=[30, 30, 30, 10, 10, 30, 12], tablefmt='simple_grid'))
+    for a in albums:
+        table.add_row(a[ARTIST], a[TRACK_ARTIST], a[ALBUM],
+                      str(a[TRACKS]), a[RELEASED], a[PLAYLIST], a[ADDED])
+
+    print(table)
 
 
-track_artist_query = f'''SELECT
-    {Columns.album_id}
-    , GROUP_CONCAT({Columns.single_artist}, ';') AS {Columns.track_artists}
-    , {Columns.album}
-FROM (SELECT alb.id AS {Columns.album_id}
-        , art.name as {Columns.single_artist}
-        , alb.name as {Columns.album}
-    FROM Track t
-    JOIN TrackArtist ta on ta.track_id = t.id
-    JOIN Artist art on art.id = ta.artist_id
-    JOIN Album alb on alb.id = t.album_id
-    WHERE art.name LIKE format('%%%s%%', ?)
-    GROUP by art.id, alb.id)
-GROUP by album_id
+def build_query(where_clause: str) -> str:
+    return f'''SELECT
+    x.artist
+    , y.track_artist
+    , alb.name AS album
+    , alb.total_tracks AS tracks
+    , substr(alb.release_date, 1, 4) as released
+    , p.name AS playlist
+    , substr(pt.added_at, 1, 10) as added
+    , alb.id AS album_id
+FROM Album alb
+JOIN (
+        SELECT aa.album_id AS album_id, art.name AS artist, art.id AS artist_id
+        FROM Album a
+        JOIN AlbumArtist aa ON aa.album_id = a.id
+        JOIN Artist art ON art.id = aa.artist_id
+        GROUP BY art.id, a.id
+    ) AS x ON x.album_id = alb.id
+JOIN (
+        SELECT art.name AS track_artist, a.id AS album_id, art.id AS track_artist_id
+        FROM Album a
+        JOIN Track t ON t.album_id = a.id
+        JOIN TrackArtist ta ON ta.track_id = t.id
+        JOIN Artist art ON art.id = ta.artist_id
+        GROUP BY art.id, a.id
+    ) AS y ON y.album_id = alb.id
+JOIN Track t ON t.album_id = alb.id
+JOIN PlaylistTrack pt ON pt.track_id = t.id
+JOIN Playlist p ON p.id = pt.playlist_id
+{where_clause}
+GROUP BY p.id, alb.id, x.artist_id, y.track_artist_id
 '''
-
-
-def build_album_artist_query(albums: dict, order_by: str = ''):
-    if not order_by:
-        order_by = f'{Columns.artists},{Columns.album}'
-
-    return f'''SELECT {Columns.album_id}
-    , GROUP_CONCAT(artist, '; ') AS {Columns.artists}
-    , {Columns.album}
-    , {Columns.tracks}
-    , {Columns.released}
-    , {Columns.playlist}
-    , {Columns.added}
-    , {Columns.playlist_id}
-FROM (SELECT alb.id AS {Columns.album_id}
-        , art.name as {Columns.single_artist}
-        , alb.name as {Columns.album}
-        , alb.total_tracks as {Columns.tracks}
-        , substr(alb.release_date, 1, 4) as {Columns.released}
-        , p.name as {Columns.playlist}
-        , substr(pt.added_at, 1, 10) as {Columns.added}
-        , p.id as {Columns.playlist_id}
-    FROM AlbumArtist aa
-    JOIN Artist art on art.id = aa.artist_id
-    JOIN Album alb on alb.id = aa.album_id
-    JOIN Track t on t.album_id = alb.id
-    JOIN PlaylistTrack pt on pt.track_id = t.id
-    JOIN Playlist p on p.id = pt.playlist_id
-    WHERE art.name LIKE format('%%%s%%', ?) OR alb.id in ({','.join(["?" for _ in albums])})
-    GROUP by p.id, art.id, alb.id)
-GROUP by playlist_id, album_id
-ORDER by {order_by};'''
 
 
 def parse_search_term(args: list[str]) -> str:
@@ -216,15 +222,9 @@ def main():
         exit()
 
     print_info(f'\n\tSearching for "{search_term}":')
-
-    cursor = get_sqlite_cursor(str(Path.home() / 'playlister.db'))
-
-    # TODO: get _all_ track artists for the matching albums, not just the artists that match the search term
-    track_matches = search_by_track_artist(cursor, search_term)
-
-    albums = search_by_album_artist(cursor, search_term, track_matches)
-
-    print_results(merge_album_data(track_matches, albums))
+    cursor = get_sqlite_cursor(sql_db)
+    albums = search_by_artist(cursor, search_term)
+    print_results(format_albums_for_table(albums), search_term)
 
     if albums:
         print_info(f'\n\t{len(albums)} match(es)')
