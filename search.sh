7fa8e7efafac8e369e07656b3a3cb5471de5c323
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-03-24 14:25:03 -0500

    move logic out of function
    making this a script to be invoked so that it's usable from fish

--- search.sh
+++ search.sh
@@ -62,102 +62,96 @@ function __sm_default_search() {
 }
 
 # TODO: use getopts to parse options
-function sm() {
+case $1 in
+# print usage info
+-h | --help | help)
+    __sm_print_usage
+    ;;
+# pass through all args to default search
+verbatim)
+    shift
+    __sm_default_search "${*}"
+    ;;
+# search sqlite DB on artist/album name
+db)
+    shift
+    ERROR="$(__sm_validate_search_term "${*}")"
+    if [[ -n "$ERROR" ]]; then
+        echo >&2 "$ERROR"
+        return 1
+    else
+        [[ -n "${*}" ]]
+        echo "Matches for '${*}':"
+        sqlite3 --readonly "$HOME/playlister.db" ".param init" ".param set :term '${*}'" ".read $SM_REPO/sql/search_playlister_db.sql"
+    fi
+    ;;
+# search sqlite DB on song titles
+song)
+    shift
+    ERROR="$(__sm_validate_search_term "${*}")"
+    if [[ -n "$ERROR" ]]; then
+        echo >&2 "$ERROR"
+        return 1
+    else
+        [[ -n "${*}" ]]
+        echo "Tracks matching '${*}':"
+        sqlite3 --readonly "$HOME/playlister.db" ".param init" ".param set :term '${*}'" ".read $SM_REPO/sql/song_search.sql"
+    fi
+    ;;
+# get last N albums added to starred Spotify playlists
+last)
+    limit=10 # default to 10
+    if [[ -n "$2" ]] && [[ "$2" -gt 0 ]]; then
+        limit=$2
+    fi
+    sqlite3 --readonly "$HOME/playlister.db" ".param init" ".param set :limit $limit" ".read $SM_REPO/sql/get_last_x_additions.sql"
+    ;;
+    # update tsv file manually
+rs)
+    starred_music_rs # binary located in ~/bin/
+    ;;
+sort)
+    shift
+    echo -e "\t--Search for '" "${@:2}" "'--\n"
     case $1 in
-    # print usage info
-    -h | --help | help)
+    date)
+        shift
+        __sm_search "${*}" | __sm_format_matches | __sm_sort_by_release
+        ;;
+        # search tsv file and sort by artist
+    artist)
+        shift
+        __sm_search "${*}" | __sm_format_matches | __sm_sort_by_release
+        ;;
+    album)
+        shift
+        __sm_search "${*}" | __sm_format_matches | __sm_sort_by_album
+        ;;
+    *)
+        echo -e "'sort' must be followed by [date | artist]\n"
         __sm_print_usage
         ;;
-    # pass through all args to default search
-    verbatim)
-        shift
-        __sm_default_search "${*}"
-        ;;
-    # search sqlite DB on artist/album name
+    esac
+    ;;
+sync)
+    case $2 in
+    # sync sqlite db
     db)
-        shift
-        ERROR="$(__sm_validate_search_term "${*}")"
-        if [[ -n "$ERROR" ]]; then
-            echo >&2 "$ERROR"
-            return 1
-        else
-            [[ -n "${*}" ]]
-            echo "Matches for '${*}':"
-            sqlite3 --readonly "$HOME/playlister.db" ".param init" ".param set :term '${*}'" ".read $SM_REPO/sql/search_playlister_db.sql"
-        fi
+        dotnet run --project "$HOME/ccb012100/playlister/Playlister.csproj" --configuration Release
         ;;
-    # search sqlite DB on song titles
-    song)
-        shift
-        ERROR="$(__sm_validate_search_term "${*}")"
-        if [[ -n "$ERROR" ]]; then
-            echo >&2 "$ERROR"
-            return 1
-        else
-            [[ -n "${*}" ]]
-            echo "Tracks matching '${*}':"
-            sqlite3 --readonly "$HOME/playlister.db" ".param init" ".param set :term '${*}'" ".read $SM_REPO/sql/song_search.sql"
-        fi
+        # sync tsv file
+    tsv)
+        "$SM_REPO/update_spreadsheet.py"
         ;;
-    # get last N albums added to starred Spotify playlists
-    last)
-        limit=10 # default to 10
-        if [[ -n "$2" ]] && [[ "$2" -gt 0 ]]; then
-            limit=$2
-        fi
-        sqlite3 --readonly "$HOME/playlister.db" ".param init" ".param set :limit $limit" ".read $SM_REPO/sql/get_last_x_additions.sql"
-        ;;
-        # update tsv file manually
-    rs)
-        starred_music_rs # binary located in ~/bin/
-        ;;
-    sort)
-        shift
-        echo -e "\tSearch for '" "${@:2}" "':\n"
-        case $1 in
-        date)
-            shift
-            __sm_search "${*}" | __sm_format_matches | __sm_sort_by_release
-            ;;
-            # search tsv file and sort by artist
-        artist)
-            shift
-            __sm_search "${*}" | __sm_format_matches | __sm_sort_by_release
-            ;;
-        album)
-            shift
-            __sm_search "${*}" | __sm_format_matches | __sm_sort_by_album
-            ;;
-        *)
-            echo -e "'sort' must be followed by [date | artist]\n"
-            __sm_print_usage
-            ;;
-        esac
-        ;;
-    sync)
-        case $2 in
-        # sync sqlite db
-        db)
-            dotnet run --project "$HOME/ccb012100/playlister/Playlister.csproj" --configuration Release
-            ;;
-            # sync tsv file
-        tsv)
-            "$SM_REPO/update_spreadsheet.py"
-            ;;
-        *)
-            echo "Error: you must use 'sync db' or 'sync tsv'"
-            return 1
-            ;;
-        esac
-        ;;
-    # search tsv file with default search
     *)
-        echo -e "\tSearch for '${*}':\n"
-        __sm_default_search "${*}"
+        echo "Error: you must use 'sync db' or 'sync tsv'"
+        return 1
         ;;
     esac
-}
-
-alias smdb='sm sync db'
-alias smtsv='sm sync tsv'
-alias smsync='sm sync db && sm sync tsv'
+    ;;
+# search tsv file with default search
+*)
+    echo -e "\t--Search for '${*}'--\n"
+    __sm_default_search "${*}"
+    ;;
+esac
