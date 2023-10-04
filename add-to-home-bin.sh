68b2330f0afdcc9bb1c511f32ace6a3d02c8e1bb
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-10-04 14:32:03 -0500

    fix path to scripts

--- add-to-home-bin.sh
+++ add-to-home-bin.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-ln -s search.py "$HOME"/bin/playlist-search.py
-ln -s search.sh "$HOME"/bin/playlist-search.sh
+set -eou pipefail
+
+repo=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+
+ln -s "$repo"/search.py "$HOME"/bin/playlist-search.py
+ln -s "$repo"/search.sh "$HOME"/bin/playlist-search.sh
