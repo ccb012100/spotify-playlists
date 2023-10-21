a6ed2637236206d81873e45dfa30da491fb58083
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-10-21 15:01:58 -0500

    update name of python script

--- scripts/add-to-home-bin.sh
+++ scripts/add-to-home-bin.sh
@@ -3,5 +3,5 @@ set -eou pipefail
 
 repo=$(dirname -- "$(readlink -f -- "$0")")
 
-ln -s "$repo"/../search.py "$HOME"/bin/playlist-search.py
+ln -s "$repo"/../dbsearch.py "$HOME"/bin/playlist-dbsearch.py
 ln -s "$repo"/../search.sh "$HOME"/bin/playlist-search.sh
